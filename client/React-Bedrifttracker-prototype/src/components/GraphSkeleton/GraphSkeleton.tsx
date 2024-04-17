497c20463b3fa5aaa789a46b03354879e7e0f149
johnb08 <johnb08.kodehode@gmail.com>
2024-04-17 11:14:25 +0000

    prettying up prototype

--- client/React-Bedrifttracker-prototype/src/components/GraphSkeleton/GraphSkeleton.tsx
+++ client/React-Bedrifttracker-prototype/src/components/GraphSkeleton/GraphSkeleton.tsx
@@ -5,13 +5,20 @@ export const GraphSkeleton = () =>{
     return(
         <>
             <div className={style.graphSkeleton}>
-                <Skeleton variant="rectangular" height={580} width={20}/>
-                <Skeleton variant="rectangular" height={200} width={100}/>
-                <Skeleton variant="rectangular" height={450} width={100}/>
-                <Skeleton variant="rectangular" height={320} width={100}/>
-                <Skeleton variant="rectangular" height={500} width={100}/>
+                <Skeleton variant="rectangular" height={595} width={5}/>
+                <Skeleton variant="rectangular" height={200} width={25}/>
+                <Skeleton variant="rectangular" height={450} width={25}/>
+                <Skeleton variant="rectangular" height={320} width={25}/>
+                <Skeleton variant="rectangular" height={500} width={25}/>
+                <Skeleton variant="rectangular" height={240} width={25}/>
+                <Skeleton variant="rectangular" height={140} width={25}/>
+                <Skeleton variant="rectangular" height={550} width={25}/>
+                <Skeleton variant="rectangular" height={430} width={25}/>
+                <Skeleton variant="rectangular" height={280} width={25}/>
+                <Skeleton variant="rectangular" height={40} width={25}/>
+                <Skeleton variant="rectangular" height={520} width={25}/>
             </div>
-            <Skeleton variant="rectangular" height={20} width={600}/>
+            <Skeleton variant="rectangular" height={5} width={600}/>
         </>
     )
 }
